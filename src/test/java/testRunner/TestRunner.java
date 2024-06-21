f1edfede33a64deea3e7f44fb26ec21f2dbb2f37
Vishnu <vishnuvelu466@gmail.com>
2024-06-21 11:27:01 +0530

    new Build6

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -1,22 +1,24 @@
 package testRunner;
 
+import org.junit.runner.RunWith;
 
-import org.testng.annotations.Test;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-	
-@Test
+@RunWith(Cucumber.class)
 @CucumberOptions(
 		features = {".//Feature/IdentifyNewBikes.feature"},
+		//features= {"@target/rerun.txt"},
 		glue = {"stepDefinition","hooks"} ,
 		plugin = {"pretty","html:reports/cucumberReport.html",
+				"rerun:target/rerun.txt",
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
-		dryRun = false,
-		monochrome = true,
-		publish =true
+		
+		dryRun = false, // checks mapping between scenario steps and step definition methods
+		monochrome = true, // to avoid junk characters in output
+		publish =true // to publish report in cucumber server
 		
 		)
-public class TestRunner extends AbstractTestNGCucumberTests{
+public class TestRunner{
 	
 }
